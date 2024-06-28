84b5b988a5f8eb66cdf670b68711bebe42d31aa2
Leo G <leogretz@gmail.com>
2024-06-28 13:06:08 -0600

    main grid

---
+++ src/app/main-grid/main-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-grid.component.html',
+  styleUrl: './main-grid.component.css'
+})
+export class MainGridComponent {
+
+}
