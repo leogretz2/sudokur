aa0b2b3ff1caa910cee20afd240eb08d401de20c
Leo G <leogretz@gmail.com>
2024-07-01 10:13:51 -0400

    fix grid, populate

--- src/app/main-grid/main-grid.component.ts
+++ src/app/main-grid/main-grid.component.ts
@@ -11,11 +11,26 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class MainGridComponent implements OnInit {
-  sudokuGrid: number[][];
+  sudokuGrid: (number|null)[][];
 
   constructor() {
-    this.sudokuGrid = Array.from({ length: 9 }, () => Array(9).fill(null));
+    const puzzle = '070000043040009610800634900094052000358460020000800530080070091902100005007040802';
+    this.sudokuGrid = this.convertPuzzleStringToGrid(puzzle)
+    // this.sudokuGrid = Array.from({ length: 9 }, () => Array(9).fill(null));
   }
 
   ngOnInit(): void {}
+
+  convertPuzzleStringToGrid(puzzle: string): (number|null)[][] {
+    const grid: (number|null)[][] = [];
+    for (let i = 0; i < 9; i++) {
+      const row: (number|null)[] = [];
+      for (let j = 0; j < 9; j++) {
+        const value = parseInt(puzzle[i * 9 + j], 10);
+        row.push(isNaN(value) ? null : value);
+      }
+      grid.push(row);
+    }
+    return grid;
+  }
 }
