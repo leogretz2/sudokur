d6709f685a04f42df1b2090c483a549b97e80ff4
Leo G <leogretz@gmail.com>
2024-07-01 05:08:07 -0400

    ff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainGridComponent } from './main-grid/main-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainGridComponent],
   templateUrl: './app.component.html',
+  // templateUrl: './main-grid/main-grid.component.html',
   styleUrl: './app.component.css'
+  // styleUrl: './main-grid/main-grid.component.css'
 })
 export class AppComponent {
   title = 'sudokur';
--- src/app/main-grid/main-grid.component.ts
+++ src/app/main-grid/main-grid.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-grid',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main-grid.component.html',
   styleUrl: './main-grid.component.css'
 })
-export class MainGridComponent {
 
+export class MainGridComponent implements OnInit {
+  sudokuGrid: number[][];
+
+  constructor() {
+    this.sudokuGrid = Array.from({ length: 9 }, () => Array(9).fill(null));
+  }
+
+  ngOnInit(): void {}
 }
